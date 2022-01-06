1f6f8fd521b05ab4dc1f9f5579cd364a2d134f31
Ah-yeong <84187163+Ah-yeong@users.noreply.github.com>
2022-01-06 12:32:45 +0900

    Update bj_2606.py

--- BaekJoon/bj_2606.py
+++ BaekJoon/bj_2606.py
@@ -1,19 +1,7 @@
+import sys
 from collections import deque
 
-def init(V):
-    visited = [False for _ in range(V+1)] 
-    return visited
-
-def insert_edge(V, E):
-    adj_mat = [[0 for _ in range(V+1)] for _ in range(V+1)]
-
-    for edge in E:
-        adj_mat[edge[0]][edge[1]] = 1
-        adj_mat[edge[1]][edge[0]] = 1
-
-    return adj_mat
-
-def bfs(V, adj_mat, vertex, visited):
+def bfs(vertex):
     cnt = 0
     visited[vertex] = True
 
@@ -30,11 +18,15 @@ def bfs(V, adj_mat, vertex, visited):
     return cnt
         
 if __name__ == "__main__":
-    V = int(input()) 
-    E = int(input()) 
-    E = [list(map(int, input().split())) for _ in range(E)] 
+    V = int(sys.stdin.readline()) 
+    E = int(sys.stdin.readline()) 
+    E = [list(map(int, sys.stdin.readline().split())) for _ in range(E)] 
     
-    adj_mat = insert_edge(V,E)
-    visited = init(V)
+    visited = [False for _ in range(V+1)] 
+    adj_mat = [[0 for _ in range(V+1)] for _ in range(V+1)]
 
-    print(bfs(V, adj_mat, 1, visited))
+    for edge in E:
+        adj_mat[edge[0]][edge[1]] = 1
+        adj_mat[edge[1]][edge[0]] = 1
+
+    print(bfs(1))
